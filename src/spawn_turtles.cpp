9f927c3076dceef9c338f5e1d98bb08b182da7cf
akeaveny <akeaveny@uwaterloo.ca>
2020-06-21 12:57:19 -0400

    ros param

--- src/spawn_turtles.cpp
+++ src/spawn_turtles.cpp
@@ -6,6 +6,11 @@
 
 // std lib
 #include <cstdlib>
+#include <string>
+
+// std::string turtle1_name_;
+// double turtle1_x_;
+// double turtle1_y_;
 
 void killturtle(const std::string &name = "turtle1") {
 
@@ -18,6 +23,10 @@ void killturtle(const std::string &name = "turtle1") {
 }
 
 void spawnturtle(float x, float y, float theta, const std::string &name) {
+
+    ROS_INFO_STREAM("Name: " << name);
+    ROS_INFO_STREAM("X: " << x);
+    ROS_INFO_STREAM("Y: " << y);
 
     ros::NodeHandle nh_;
     ros::ServiceClient spawnturtle = nh_.serviceClient<turtlesim::Spawn>("/spawn");
@@ -35,11 +44,36 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "spawn_turtles");
     ros::NodeHandle nh;
 
-
     // ====== clear ======   
     killturtle();
 
+    XmlRpc::XmlRpcValue turtles_;
+    nh.getParam("/uwr_software_challenge/spawn_turtles/turtles", turtles_);
+
+    for (int turtle_index = 0; turtle_index < turtles_.size(); turtle_index++) {
+        ROS_ASSERT(turtles_[turtle_index].getType() == XmlRpc::XmlRpcValue::TypeStruct);
+
+        ROS_ASSERT(turtles_[turtle_index].hasMember("name"));
+        ROS_ASSERT(turtles_[turtle_index]["name"].getType() == XmlRpc::XmlRpcValue::TypeString);
+        std::string turtle_name_ = turtles_[turtle_index]["name"];
+
+        ROS_ASSERT(turtles_[turtle_index].hasMember("x"));
+        ROS_ASSERT(turtles_[turtle_index]["x"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+        double turtle_x_ = turtles_[turtle_index]["x"];
+
+        ROS_ASSERT(turtles_[turtle_index].hasMember("y"));
+        ROS_ASSERT(turtles_[turtle_index]["y"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+        double turtle_y_ = turtles_[turtle_index]["y"];
+
+        // ====== spawn ====== 
+        spawnturtle(turtle_x_, turtle_y_, 0, turtle_name_);
+        
+    }
+
+    // nh.getParam("/uwr_software_challenge/spawn_turtles/turtle1_x_", turtle1_x_);
+    // nh.getParam("/uwr_software_challenge/spawn_turtles/turtle1_y_", turtle1_y_);
+
     // ====== spawn ====== 
-    spawnturtle(5, 5, 0, "stationary_turtle");
+    // spawnturtle(turtle1_x_, turtle1_y_, 0, turtle1_name_);
     spawnturtle(25, 10, 0, "moving_turtle");
 }
